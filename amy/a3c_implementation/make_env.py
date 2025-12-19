2fbbd247741ba7aa118c1b09b131452d6ddef3ff
Amy-RT <95489289+Amy-RT@users.noreply.github.com>
2025-12-19 17:04:55 +0000

    clean up make_env.py

--- amy/a3c_implementation/make_env.py
+++ amy/a3c_implementation/make_env.py
@@ -1,47 +1,8 @@
-import os
 import gymnasium as gym
-import gym4real
 from gym4real.envs.wds.utils import parameter_generator
 # from gym4real.envs.wds.hourly_wrapper import HourlyDecisionWrapper - not using right ? 
 from gym4real.envs.wds.reward_scaling_wrapper import RewardScalingWrapper
 from stable_baselines3.common.vec_env import DummyVecEnv, VecMonitor, VecNormalize
-
-
-# ***** will debug why the implementation isn't working with the wrappers 
-# current implementation works with unwrapped env - getting episode rewards of 140 / 167 - with lows of 72 
-# (expected for A3C performance to fluctuate according to literature)
-
-
-# def make_env(
-#     # use_wrapper: bool,
-#     use_normalisation: bool,
-#     reward_scaling: bool = False,
-# ):
-    # def _init():
-    #     package_root = os.path.dirname(gym4real.__file__)
-    #     world_file = os.path.join(
-    #         package_root, "envs", "wds", "world_anytown_fixed.yaml"     # CHANGE TO YOUR YAML
-    #     )
-
-    #     params = parameter_generator(world_file)
-    #     env = gym.make("gym4real/wds-v0", settings=params)
-
-    #     # ADDRESS THIS TypeError: 'RewardScalingWrapper' object is not callable
-    #     # if reward_scaling:
-    #     #     env = RewardScalingWrapper(env) 
-
-    #     # if use_wrapper:
-    #     #     env = HourlyDecisionWrapper(env)
-
-    #     return env
-
-    # env = DummyVecEnv([_init()]) #added brackets here - otherwise results in NoneType object
-    # env = VecMonitor(env)
-
-    # if use_normalisation:
-    #     env = VecNormalize(env, norm_obs=True, norm_reward=True)
-
-    # return env
 
 def make_env(use_normalisation: bool,
                 reward_scaling: bool = False,):
@@ -60,13 +21,13 @@ def make_env(use_normalisation: bool,
     if reward_scaling:
         env = RewardScalingWrapper(env) 
 
-    
+    # currently debugging this 
     # if use_normalisation:
         # triggers TypeError: 'int' object is not subscriptable - when performing the agent's chosen action on the env
-        # # test to normalise rewards and observations 
+        # test to normalise rewards and observations 
         # env = DummyVecEnv([lambda: env])
         # env = VecMonitor(env)
         # env = VecNormalize(env, norm_obs=True, norm_reward=True)
-        # # end of code implementing normalisation test
+        # end of code implementing normalisation test
 
     return env
