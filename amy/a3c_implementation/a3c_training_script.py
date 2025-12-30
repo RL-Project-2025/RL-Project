964cfe96cc4af464595a3fea6ea6ccfce365389f
Amy-RT <95489289+Amy-RT@users.noreply.github.com>
2025-12-30 11:09:18 +0000

    Add boolean flag for logging for ease of use

--- amy/a3c_implementation/a3c_training_script.py
+++ amy/a3c_implementation/a3c_training_script.py
@@ -13,17 +13,19 @@ if __name__ == '__main__': # 'if clause protection' needed here otherwise it tri
         # current process has finished its bootstrapping phase.
     # as explained by https://docs.pytorch.org/docs/stable/notes/windows.html#multiprocessing-error-without-if-clause-protection 
 
-    # **** HYPERPARAMETERS
-    MAX_EPISODE_COUNT = 100 #safety net to prevent infinite looping
+    # ******************** HYPERPARAMETERS
+    MAX_EPISODE_COUNT = 200000 #safety net to prevent infinite looping
     #the paper introducing A3C suggests global agent should be updated every 5 actions
     # ref section 8 "Experimental setup" of the paper: https://arxiv.org/pdf/1602.01783 
     GLOBAL_AGENT_UPDATE_INTERVAL = 5 #tried 20 but no performance improvements 
     GAMMA = 0.99 #place more emphasis on long term outcomes
     LEARNING_RATE = 1e-4
     NUM_LOCAL_AGENTS = mp.cpu_count() #might need to limit here for HEX
+
     IS_NORMALISING_REWARDS = True
     IS_SCALING_REWARDS = True
     IS_USING_EMA = False
+    IS_TB_LOGGING = True
     # ********************
 
     # set up tensorboard logging 
@@ -64,7 +66,7 @@ if __name__ == '__main__': # 'if clause protection' needed here otherwise it tri
                                 is_using_ema = IS_USING_EMA,
                                 max_episode_count = MAX_EPISODE_COUNT,
                                 global_update_interval = GLOBAL_AGENT_UPDATE_INTERVAL,
-                                is_logging = False,
+                                is_logging = IS_TB_LOGGING,
                                 log_dir = log_dir,
                                 logging_run_name = run_name) 
                     for i in range(NUM_LOCAL_AGENTS)]
