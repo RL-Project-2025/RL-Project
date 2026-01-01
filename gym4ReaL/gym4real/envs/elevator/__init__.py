1b8367bab5c1e26018ca0b730c4600660f20f718
hulusierin <hulusierin@gmail.com>
2026-01-01 08:47:57 +0000

    Frogot the other models(will replace them either way)

--- gym4ReaL/gym4real/envs/elevator/__init__.py
+++
@@ -1,2 +0,0 @@
-from gymnasium.envs.registration import register
-
--- gym4ReaL/gym4real/envs/microgrid/simulator/__init__.py
+++
@@ -1,4 +0,0 @@
-from .generation import PVGenerator, DummyGenerator
-from .demand import EnergyDemand
-from .market import EnergyMarket, DummyMarket
-from .ambient_temp import AmbientTemperature, DummyAmbientTemperature
